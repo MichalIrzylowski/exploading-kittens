a48681df10d2c37e1c8638ed7347b97446dba631
Michał Irzyłowski <michal.irzylowski@gmail.com>
2020-05-28 07:59:22 +0200

    + Add: [] save work

--- src/front/pages/board/board.tsx
+++ src/front/pages/board/board.tsx
@@ -1,30 +1,42 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 import { useWebSocket } from '@front/contexts/main-websocket';
 import { sessionStorageItems } from '@front/shared/types';
 import { LayoutWrapper } from '@front/components/layout-wrapper';
 
 import { payloadTypes } from '@shared/payload-types';
+import { homePage } from '@shared/urls';
 
 import { BoardCreator } from './board-creator';
 import { PlayerView } from './player-view';
 
 export const Board = () => {
-  const location = useLocation();
-  console.log(location.state);
-  const [isBoard, setBoard] = useState(location.state ? location.state : '');
+  const history = useHistory();
+  history.location.state;
+  const [isBoard, setBoard] = useState(history.location.state ? history.location.state : '');
   const ws = useWebSocket();
 
   useEffect(() => {
-    if (location.state) {
+    if (history.location.state) {
       const playerId = sessionStorage.getItem(sessionStorageItems.user) as string;
       ws.send(payloadTypes.joinGame, {
-        boardId: location.state,
+        boardId: history.location.state,
         userId: JSON.parse(playerId).id,
       });
     }
-  });
+
+    history.listen((location) => {
+      console.log('change route');
+      if (location.pathname === homePage) {
+        const player = sessionStorage.getItem(sessionStorageItems.user) as string;
+        const playerId = JSON.parse(player).id;
+        ws.send(payloadTypes.leaveGame, { playerId });
+
+        sessionStorage.removeItem(sessionStorageItems.currentGame);
+      }
+    });
+  }, []);
   return (
     <main>
       <LayoutWrapper>
